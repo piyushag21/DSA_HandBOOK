43e8a9b9a30ec2a68444710b16ab081258e69c96
piyushag21 <piyush.ag.21@gmail.com>
2021-04-02 23:35:06 +0530

    INIT

--- app/src/main/java/com/example/rebuilt3/ui/MainActivity3.java
+++ app/src/main/java/com/example/rebuilt3/ui/MainActivity3.java
@@ -2,15 +2,41 @@ package com.example.rebuilt3.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.rebuilt3.R;
+import com.google.android.material.textfield.TextInputEditText;
 
-public class MainActivity3 extends AppCompatActivity {
-
+public class MainActivity3 extends AppCompatActivity
+{
+    TextInputEditText textEmail,textPassword;
+    ProgressBar progressBar;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        textEmail = (TextInputEditText)findViewById(R.id.email_login);
+        textPassword = (TextInputEditText)findViewById(R.id.password_login);
+        progressBar = (ProgressBar)findViewById(R.id.progressbarLogin);
+    }
+
+    public void LoginUser(View v)
+    {
+
+    }
+    public void gotoRegister(View v)
+    {
+        Intent i = new Intent(MainActivity3.this, RegisterActivity.class);
+        startActivity(i);
+    }
+
+    public void forgotPassword(View v)
+    {
+
     }
 }
--- app/src/main/java/com/example/rebuilt3/ui/RegisterActivity.java
+++ app/src/main/java/com/example/rebuilt3/ui/RegisterActivity.java
@@ -2,15 +2,37 @@ package com.example.rebuilt3.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.rebuilt3.R;
+import com.google.android.material.textfield.TextInputEditText;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity
+{
+    TextInputEditText textEmail,textPassword;
+    ProgressBar progressBar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        textEmail = (TextInputEditText)findViewById(R.id.email_register);
+        textPassword = (TextInputEditText)findViewById(R.id.password_register);
+        progressBar = (ProgressBar)findViewById(R.id.progressBarRegister);
+    }
+
+    public void RegisterUser(View v)
+    {
+
+    }
+    public void gotoLogin(View v)
+    {
+        Intent i = new Intent(RegisterActivity.this, MainActivity3.class);
+        startActivity(i);
     }
 }
