600437be9967068d3beab8c4569444a25daeb456
piyushag21 <piyush.ag.21@gmail.com>
2021-04-02 17:49:59 +0530

    Init

--- app/src/main/java/com/example/rebuilt3/ui/RegisterActivity.java
+++ app/src/main/java/com/example/rebuilt3/ui/RegisterActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.rebuilt3.R;
 
-public class MainActivity3 extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_register);
     }
 }
